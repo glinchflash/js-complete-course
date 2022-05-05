ccb948a9487aa1bb5809326d11225f78d95719f5
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-05-05 17:00:36 +0200

    finished excercise 3.5(optional)

--- 03-dates/05-get-spooky-fridays-Optional/script.js
+++ 03-dates/05-get-spooky-fridays-Optional/script.js
@@ -1,16 +1,43 @@
-/* becode/javascript
- *
- * /04-dates/05-get-spooky-fridays/script.js - 4.5: calcul des vendredi 13
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
+(function () {
+    const btnRun = document.getElementById('run');
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const targetday = 13;
+    let input = document.getElementById('year');
+    btnRun.addEventListener('click', calc)
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
-(function() {
+    let date = new Date();
+    function calc() {
 
-    // your code here
 
+        let input2 = input.value;
+
+
+        let monthsWithf13 = [];
+
+        date.setFullYear(input2);
+
+        date.setDate(targetday);
+        for (month = 0; month < months.length; month++) {
+
+            date.setMonth(month)
+
+            if (date.getDay() === 5) {
+                monthsWithf13.push(months[month])
+            }
+        }
+
+        let result = "In" + input2 + ": " ;
+
+        for (i = 0; i < monthsWithf13.length; i++) {
+
+            result += monthsWithf13[i] + ",";
+        }
+        if (monthsWithf13.length > 1) {
+            result += " have a friday the 13th";
+        }else{
+            result += " has a friday the 13th";
+        }
+        alert(result);
+    }
 })();

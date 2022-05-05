549a77acfdea14ebe49714518040c7c5883103f3
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-05-05 14:50:20 +0200

    finished excercise 3.3

--- 03-dates/03-age-by-select/script.js
+++ 03-dates/03-age-by-select/script.js
@@ -10,7 +10,36 @@
 // You will have time to focus on it later.
 
 (function() {
+    let btnrun = document.getElementById("run");
 
-    // your code here
+    btnrun.addEventListener('click',age);
+    function age() {
+        let d1 = document.getElementById('dob-day').value;
+        let m1 = document.getElementById('dob-month').value;
+        let y1 = document.getElementById('dob-year').value;
+
+        let date = new Date();
+        let d2 = date.getDate();
+        let m2 = 1 + date.getMonth();   
+        let y2 = date.getFullYear();
+        let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+        if(d1 > d2){
+            d2 = d2 + month[m2 - 1];
+            m2 = m2 - 1;
+        }
+        if(m1 > m2){
+            m2 = m2 + 12;
+            y2 = y2 - 1;
+        }
+        let d = d2 - d1; /* to calc days aswell */
+        let m = m2 - m1;/* to calc months aswell */
+        let y = y2 - y1;
+
+        alert('Your Age is '+y+' Years ');
+
+    }
+
+
 
 })();

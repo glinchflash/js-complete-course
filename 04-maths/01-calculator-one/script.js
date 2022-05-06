136e4b5a4b33e0fcebd78908670fe42fc852f0d9
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-05-06 09:30:42 +0200

    excercise 4.1 finished

--- 04-maths/01-calculator-one/script.js
+++ 04-maths/01-calculator-one/script.js
@@ -13,18 +13,30 @@
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("addition").addEventListener("click", function() {
-        // perform an addition
+       let one = document.getElementById('op-one').value;
+       let two =  document.getElementById('op-two').value;
+       let result = Number(one) + Number(two);
+       alert(result);
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
-        // perform an substraction
+        let one = document.getElementById('op-one').value;
+        let two =  document.getElementById('op-two').value;
+        let result = one - two;
+        alert(result);
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
-        // perform an multiplication
+        let one = document.getElementById('op-one').value;
+        let two =  document.getElementById('op-two').value;
+        let result = one * two;
+        alert(result);
     });
 
     document.getElementById("division").addEventListener("click", function() {
-        // perform an division
+        let one = document.getElementById('op-one').value;
+        let two =  document.getElementById('op-two').value;
+        let result = one / two;
+        alert(result);
     });
 })();

80008fa71f67b35367137a835b5bcb18b76b7575
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-05-06 11:54:54 +0200

    finished chapter 4

--- 04-maths/06-factorial-Optional/script.js
+++ 04-maths/06-factorial-Optional/script.js
@@ -9,13 +9,17 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-    
+(function () {
+
     // to get the value of an input: document.getElementById("element-id").value
 
-    document.getElementById("run").addEventListener("click", function() {
-
-        // your code here
+    document.getElementById("run").addEventListener("click", function () {
+        let number = document.getElementById('number').value;
+        let factorial = 1;
+        for (i = 1; i <= number; i++) {
+            factorial = i * factorial;
+            console.log(factorial);
+        }
 
     });
 

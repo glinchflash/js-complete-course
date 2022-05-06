75704ae6980318c3c802f7209e2612a73c215a8a
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-05-06 10:09:22 +0200

    finished excercise 4.2

--- 04-maths/02-calculator-two/script.js
+++ 04-maths/02-calculator-two/script.js
@@ -9,15 +9,39 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
     // to get the value of an input: document.getElementById("element-id").value
+    const one = document.getElementById('op-one');
+    const two = document.getElementById('op-two');
 
-    var performOperation = function(operation) {
-        // perform the operation
+    var performOperation = function (operation) {
+        const opOne = parseInt(one.value);
+        const opTwo = parseInt(two.value);
+        let result = 0;
+        switch (operation) {
+            case "addition":
+                result = (opOne + opTwo);
+                alert(result);
+                break;
+            case "substraction":
+                result = (opOne - opTwo);
+                alert(result);
+                break;
+            case "multiplication":
+                result = (opOne * opTwo);
+                alert(result);
+                break;
+            case "division":
+                result = (opOne / opTwo);
+                alert(result);
+                break;
+            default:
+                break;
+        }
     };
 
-    Array.from(document.querySelectorAll("button.operator")).forEach(function($btn) {
-        $btn.addEventListener("click", function() {
+    Array.from(document.querySelectorAll("button.operator")).forEach(function ($btn) {
+        $btn.addEventListener("click", function () {
             performOperation($btn.id);
         });
     });

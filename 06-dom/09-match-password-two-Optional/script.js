6b5063365d6b29445dcfe6116cf4f1850d3582bc
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-05-10 11:46:26 +0200

    finished excercise 6.9

--- 06-dom/09-match-password-two-Optional/script.js
+++ 06-dom/09-match-password-two-Optional/script.js
@@ -11,6 +11,19 @@
 
 (function() {
 
-    // your code here
+    document.getElementById("run").addEventListener("click" , () => {
+        let pass1 = document.getElementById('pass-one');
+        let pass2 = document.getElementById('pass-two');
+
+        if (pass1.value === pass2.value){
+            alert("passwords matches perfecltly!")
+        } else{
+            pass1.classList.add("error");
+            pass2.classList.add("error");
+        }
+
+
+    })
+
 
 })();

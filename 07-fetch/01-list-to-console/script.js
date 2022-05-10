6ef5bff01ac55c2a25a98715bb5bb45f55a2556b
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-05-10 15:07:07 +0200

    finished 7.1 & 7.2

--- 07-fetch/01-list-to-console/script.js
+++ 07-fetch/01-list-to-console/script.js
@@ -10,5 +10,10 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    document.getElementById("run").addEventListener("click",() => {
+        fetch("../../_shared/api.json")
+            .then(res => res.json())
+            .then(data => console.log(data));
+    });
+
 })();
--- 07-fetch/02-details/script.js
+++ 07-fetch/02-details/script.js
@@ -10,5 +10,28 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    document.getElementById("run").addEventListener("click", () => {
+        //linking api to js
+        fetch("../../_shared/api.json")
+            .then(res => res.json())
+            .then(data => {
+        //get id from api and import information to js
+                const heroId = +document.getElementById("hero-id").value;
+                const hero = data.heroes.find(x => x.id ===heroId);
+                console.log(hero);
+        //get template and clone it's content (template.content.cloneNode)
+                let template = document.getElementById('tpl-hero');
+                let clone = template.content.cloneNode(true);
+        //link name,alter-ego and power to cloned template
+                const name = clone.querySelector(".name");
+                const alterEgo = clone.querySelector(".alter-ego");
+                const power = clone.querySelector(".powers");
+        //write/fill template
+                name.innerText = hero.name;
+                alterEgo.innerText = hero.alterEgo;
+                power.innerText = hero.abilities.join(', ');
+        //implement "cloned template" into "target"
+                document.getElementById("target").appendChild(clone);
+            });
+    });
 })();

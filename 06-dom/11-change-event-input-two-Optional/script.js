1335ba4ccefb8131a6425cb2dfe4d4ac0603927b
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-05-10 12:11:20 +0200

    finished excercise 6.11

--- 06-dom/11-change-event-input-two-Optional/script.js
+++ 06-dom/11-change-event-input-two-Optional/script.js
@@ -9,8 +9,18 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    // your code here
 
+    document.getElementById('pass-one').minLength = '8';
+    document.getElementById('pass-one').addEventListener('input', () => {
+        let password = document.getElementById('pass-one').value;
+        if (password.length < 8) {
+            document.getElementById('validity').innerHTML = "Password lenght must be atleast 8 characters"
+        } else if (!/(?=.*\d*2)/.test(password)) {
+            document.getElementById('validity').innerHTML = 'Password has to contain atleast 2 numbers';
+        } else {
+            document.getElementById('validity').innerHTML = 'Ok';
+        }
+    })
 })();

1e896312c5c4947cca57f8f3d1ed13d7d6212210
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-05-10 09:28:26 +0200

    finished chapter 5 Arrays

--- 05-arrays/11-rand-array-stats-Optional/script.js
+++ 05-arrays/11-rand-array-stats-Optional/script.js
@@ -11,6 +11,25 @@
 
 (function() {
 
-    // your code here
+    document.getElementById("run").addEventListener("click" , () =>{
+        let randomArray = Array(10).fill().map(()=> Math.floor(Math.random()* 100));
+        randomArray.forEach( (number, index) => {
+            let i = index + 1 ;
+            document.getElementById(`n-${i}`).innerHTML = number ;
+        });
+        /*---- Min number ----*/
+        document.getElementById('min').innerHTML = randomArray.reduce((a , b) => Math.min(a,b));
+        /*----Max number ----*/
+        document.getElementById('max').innerHTML = randomArray.reduce((a,b) => Math.max(a,b));
+        /*----- sum ---------*/
+        let sum = randomArray.reduce(function (previousValue,currentValue) {
+             return previousValue + currentValue;
+        })
+        document.getElementById('sum').innerHTML = sum;
+
+        /*----- avg ---------*/
+        document.getElementById('average').innerHTML = sum / randomArray.length;
+
+    });
 
 })();
